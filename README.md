65ea1091dfcb8ec455f036ba59b0b36816281f44
KT Watson <terrence.watson@mheducation.com>
2019-01-19 12:58:26 -0500

    improvements

---
+++ README.md
@@ -0,0 +1,21 @@
+MBTA API TypeScript demo app
+===================
+This is a demonstration app showing how you can use Node with TypeScript.
+
+Installation
+------------
+Clone the repo, then:
+```
+$ npm install
+$ npm install -g ts-node typescript
+```
+That should do it.
+
+Running It
+-------------
+There are a few different run scripts in the `package.json` file.
+
+`npm start` will start the server in dev mode, using ts-node and nodemon. On every change, ts-node will recompile the code and the server will restart. For this to work, ts-node must be installed globally (as in the instructions above.)
+
+`npm run build` will run the TypeScript compiler over the code and produce a bundle of plain JavaScript that can be run directly using NodeJs. The bundle will be set up in `/dist` directory.
+
--- src/index.ts
+++ src/index.ts
@@ -22,10 +22,12 @@ interface MbtaPredictionResult {
 }
 
 const API_URL = "https://api-v3.mbta.com";
-const watertownLat = "42.36546";
-const watertownLong = "-71.18564";
+const watertownLat = "42.36546";            // approx. latitude of the Watertown Square stop
+const watertownLong = "-71.18564";          // approx. longitude of the Watertown Square stop
+const routeNumber = "71";                   // the number of the Watertown -> Harvard route 
 
 const app = new Koa();
+
 
 const getPredictions = async () => {
     const url = `/predictions?filter[latitude]=${watertownLat}&filter[longitude]=${watertownLong}&include=stop,route,trip,schedule`
@@ -40,7 +42,7 @@ app.use( async ctx => {
 
     // predictions is a giant object. We only care about route 71, the Watertown -> Harvard route, and only the first stop, where I get on
     const route71Predictions = predictions.data.filter( prediction => {
-        return prediction.relationships.route.data.id === "71" && prediction.attributes.stop_sequence === 1;
+        return prediction.relationships.route.data.id === routeNumber && prediction.attributes.stop_sequence === 1;
     });
 
 
@@ -54,13 +56,14 @@ app.use( async ctx => {
 
     let departureTimesJS: moment.Moment[];
 
-    // Conver all the times from ISO to moment date objects for better sorting and display
+    // Convert all the times from ISO to moment date objects for better sorting and display
+    // Note we could have used JavaScript date objects here but the moment object is more flexible.
     departureTimesJS = departureTimes.map ( departure => {
         return moment(departure);
     })
 
     // sort the list of arrival times
-    departureTimesJS.sort( (date1, date2) => {
+    departureTimesJS.sort( (date1: moment.Moment, date2: moment.Moment) => {
         if(date1.isBefore(date2)) {
             return -1;
         } else if (date1.isAfter(date2)) {
@@ -69,11 +72,15 @@ app.use( async ctx => {
 
         return 0;
     });
-    
+
+    // map departure times to nice looking localized strings for better display
     const departureTimesAsLocalizedString = departureTimesJS.map ( departure => departure.format('dddd, MMMM Do YYYY, h:mm:ss a'));
 
+    
     ctx.body = departureTimesAsLocalizedString;
 
 })
 
-app.listen(8222);
+app.listen(8222, () => {
+    console.log("Listening on " + 8222);
+});
